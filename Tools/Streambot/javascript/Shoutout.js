fdeb9e0b715d4f0cba541314714494a22d1c143e
dobbsworks <63677372+dobbsworks@users.noreply.github.com>
2021-02-05 13:24:24 -0600

    shoutout category

--- Tools/Streambot/javascript/Shoutout.js
+++ Tools/Streambot/javascript/Shoutout.js
@@ -5,17 +5,18 @@ function CommandShoutout(user, args) {
     if (targetUser) {
         targetUser = targetUser.replace("@","");
         let link = `twitch.tv/${targetUser}`;
+        let shoutoutMessage = `Shout-out to ${targetUser}! Watch them at ${link}`;
         let specialShout = StorageHandler.shoutout.getUser(targetUser);
         if (specialShout && specialShout.text) {
             let text = specialShout.text;
             text = text.replace("$link", link);
             text = text.replace("$name", targetUser);
-            return text;
-        } else {
-            return `Shout-out to ${targetUser}! Watch them at ${link}`;
+            shoutoutMessage = text;
         }
+        ShoutoutAppendCategory(targetUser, shoutoutMessage);
+        return {success: true};
     } else {
-        return "WOOO";
+        return {success: true, message: "WOOO"};
     }
 }
 
@@ -25,4 +26,19 @@ function CommandSetShoutout(user, args) {
     let record = {username: targetUser, text: shoutoutText};
     StorageHandler.shoutout.upsert(record);
     return `Shout-out registered for ${targetUser}.`;
+}
+
+function ShoutoutAppendCategory(username, baseText) {
+    let request = new XMLHttpRequest();
+	let url = `https://decapi.me/twitch/game/${username}`;
+	request.open("GET", url, true);
+	request.onload = () => {
+        let category = request.responseText;
+        if (category.indexOf("No user with the name") > -1) {
+            category = "Unknown category";
+        }
+        let shoutout = baseText + ` (${category})`;
+        WriteMessage(shoutout);
+	}
+	request.send();
 }
