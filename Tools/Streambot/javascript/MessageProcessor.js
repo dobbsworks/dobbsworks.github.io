edf0472152dd3bbaaa503765d0c5c980609f5fac
dobbsworks <63677372+dobbsworks@users.noreply.github.com>
2021-03-11 08:47:01 -0600

    ban manofsteel after chat/follow

--- Tools/Streambot/javascript/MessageProcessor.js
+++ Tools/Streambot/javascript/MessageProcessor.js
@@ -6,7 +6,7 @@
 /////////////////////////////////////////////////
 
 function ProcessMessages() {
-	let chatMessages = Array.from(document.getElementsByClassName("chat-line__message")).filter(m => !m.classList.contains("processed")); 
+	let chatMessages = Array.from(document.getElementsByClassName("chat-line__message")).filter(m => !m.classList.contains("processed"));
 	for (let message of chatMessages) {
 		ProcessChatMessage(message, false);
 	}
@@ -14,7 +14,7 @@ function ProcessMessages() {
 	for (let message of rewardMessages) {
 		ProcessChatMessage(message, true);
 	}
-	
+
 	let activityToProcess = FindActivityMessagesToProcess();
 	for (let message of activityToProcess) {
 		ProcessActivityMessage(message);
@@ -23,46 +23,60 @@ function ProcessMessages() {
 function ProcessChatMessage(messageEl, isReward) {
 	let textFragments = Array.from(messageEl.querySelectorAll(".text-fragment, .chat-image, .mention-fragment"));
 	let stitchedText = textFragments.map(x => x.innerText || x.alt).join("");
-	let selectedReward = messageEl.children[0].innerText.split("\n")[0].replace("Redeemed ","");
+	let selectedReward = messageEl.children[0].innerText.split("\n")[0].replace("Redeemed ", "");
 	let badges = Array.from(messageEl.getElementsByClassName("chat-badge")).map(x => x.alt);
-	
+
 	let username = "";
 	let usernameEl = messageEl.getElementsByClassName("chat-line__username");
 	if (usernameEl.length) {
 		username = usernameEl[0].innerText;
 	} else {
 		username = selectedReward.split(" ")[0];
-		selectedReward = selectedReward.replace(username + " redeemed ","");
+		selectedReward = selectedReward.replace(username + " redeemed ", "");
 	}
-	
-	LogChatMessage({timestamp: new Date(), text: stitchedText, username: username, reward: isReward ? selectedReward : ""});
 
-	// adding kryptonite_bot, but if it ever goes rogue here's an alternate:
-	// if (username.toLowerCase().startsWith("manofsteel")) {
-	// 	setTimeout(() => {
-	// 		WriteMessageRaw("/ban " + username);
-	// 	},100)
-	// }
+	LogChatMessage({ timestamp: new Date(), text: stitchedText, username: username, reward: isReward ? selectedReward : "" });
+
+	// ban manofsteel
+	Kryptonite(username);
 
 	// Need to mark as present BEFORE processing command
 	MarkUserAsPresent(username);
-	
+
 	ProcessCommand(username, stitchedText, false, badges);
 	if (selectedReward.length > 0) {
-		ProcessCommand(username, "!" + selectedReward.replace(/ /g,'') + " " + stitchedText, true, badges);
+		ProcessCommand(username, "!" + selectedReward.replace(/ /g, '') + " " + stitchedText, true, badges);
 	}
 	messageEl.classList.add("processed");
 
-	let bitTotal = Array.from(messageEl.querySelectorAll(".chat-line__message--cheer-amount")).map(x => +(x.innerText)).reduce((a,b)=>a+b,0);
+	let bitTotal = Array.from(messageEl.querySelectorAll(".chat-line__message--cheer-amount")).map(x => +(x.innerText)).reduce((a, b) => a + b, 0);
 	if (bitTotal >= 5) {
 		let trimmed = stitchedText;
-		let cheerTypes = ["Cheer", "Pogchamp", "BibleThump", "cheerwhal", "Corgo","uni","ShowLove","Party","SeemsGood","Pride","Kappa","FrankerZ","HeyGuys","DansGame","EleGiggle","TriHard","Kreygasm","4Head","SwiftRage","NotLikeThis","FailFish","VoHiYo","PJSalt","MrDestructoid","bday","RIPCheer","Shamrock"];
+		let cheerTypes = ["Cheer", "Pogchamp", "BibleThump", "cheerwhal", "Corgo", "uni", "ShowLove", "Party", "SeemsGood", "Pride", "Kappa", "FrankerZ", "HeyGuys", "DansGame", "EleGiggle", "TriHard", "Kreygasm", "4Head", "SwiftRage", "NotLikeThis", "FailFish", "VoHiYo", "PJSalt", "MrDestructoid", "bday", "RIPCheer", "Shamrock"];
 		for (let cheerType of cheerTypes) {
 			let myRe = new RegExp(cheerType + ' [\d]*', 'g');
 			trimmed = trimmed.replace(myRe, "")
 		}
 		TTSMessage(trimmed);
 	}
+}
+
+var bannedUsernames = ["manofsteel", "mrsteal", "ladyvalkyrie", "mannasteal", "certinalynot_mos"];
+function Kryptonite(username) {
+	// check username against known manofsteel account patterns, ban
+	if (!username) return false;
+	username = username.toLowerCase();
+	for (let bannedUsername of bannedUsernames) {
+		let target = bannedUsername.toLowerCase();
+		// search username for substring
+		if (username.indexOf(target) > -1) {
+			setTimeout(() => {
+				WriteMessageRaw("/ban " + username);
+			}, 100)
+			return true;
+		}
+	}
+	return false;
 }
 
 function ProcessCommand(username, commandText, isReward, badges) {
@@ -73,10 +87,10 @@ function ProcessCommand(username, commandText, isReward, badges) {
 		isMod: badges && badges.some(x => x.toLowerCase().indexOf("moderator") > -1)
 	};
 	if (commandText.length < 2) return;
-    let commandArgs = commandText.split(" ");
-	let commandName = commandArgs.splice(0,1)[0].toLowerCase();
+	let commandArgs = commandText.split(" ");
+	let commandName = commandArgs.splice(0, 1)[0].toLowerCase();
 	if (commandName[0] !== "!") return;
-	let matchingCommands = GetCommandsByName(commandName.replace("!",""));
+	let matchingCommands = GetCommandsByName(commandName.replace("!", ""));
 
 	if (matchingCommands.length === 0) {
 		StorageHandler.invalidCommand.push({
@@ -86,11 +100,11 @@ function ProcessCommand(username, commandText, isReward, badges) {
 		})
 	}
 
-    for (let command of matchingCommands) {
+	for (let command of matchingCommands) {
 		let hasValidPermission = false;
-		
+
 		if (command.permissions === commandPermission.all) hasValidPermission = true;
-		
+
 		if (command.permissions === commandPermission.subscriber) {
 			if (user.isSub) hasValidPermission = true;
 		}
@@ -101,8 +115,8 @@ function ProcessCommand(username, commandText, isReward, badges) {
 			if (isReward) hasValidPermission = true;
 		}
 		if (username === streamerName) hasValidPermission = true;
-		
-        if (hasValidPermission) {
+
+		if (hasValidPermission) {
 			try {
 
 				if (command.cost) {
@@ -133,13 +147,13 @@ function ProcessCommand(username, commandText, isReward, badges) {
 					responseText = "@" + username + ", " + responseText;
 				}
 				if (responseText) WriteMessage(responseText);
-			} catch(err) {
+			} catch (err) {
 				console.error(err);
 			}
 			// run first valid command
 			break;
-        }
-    }
+		}
+	}
 }
 
 function WriteMessage(message) {
@@ -151,26 +165,26 @@ function WriteMessageRaw(message) {
 	let chatButton = [...document.getElementsByTagName('button')].find(x => x.innerText === "Chat");
 
 	SetInputValue(chatInput, message);
-	
+
 	chatButton.click();
 }
 
 function SetInputValue(inputEl, value) {
 	// simulates user input to work with React
-    var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLTextAreaElement.prototype, "value").set;
-    nativeInputValueSetter.call(inputEl, value);
+	var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLTextAreaElement.prototype, "value").set;
+	nativeInputValueSetter.call(inputEl, value);
 
-    var ev2 = new Event('input', { bubbles: true});
-    inputEl.dispatchEvent(ev2);
+	var ev2 = new Event('input', { bubbles: true });
+	inputEl.dispatchEvent(ev2);
 }
 
 function OnBroadcastMessage(message) {
-    // messages from the queue window panel
-    ProcessCommand(streamerName, message.data, false, []);
+	// messages from the queue window panel
+	ProcessCommand(streamerName, message.data, false, []);
 }
 
 function FindActivityMessagesToProcess() {
-	let messages = Array.from(document.getElementsByClassName("activity-base-list-item")); 
+	let messages = Array.from(document.getElementsByClassName("activity-base-list-item"));
 	let toProcess = messages.filter(m => !m.classList.contains("processed"));
 	return toProcess;
 }
@@ -183,8 +197,11 @@ function ProcessActivityMessage(messageEl) {
 		// Follows
 		if (line2 === "Followed you") {
 			let user = line1;
-			let alertText = user + " is now following!";
-			marqueeWindow.AddAlert(alertText, "rgb(0,255,128)");
+			let userIsBanned = Kryptonite(username);
+			if (!userIsBanned) {
+				let alertText = user + " is now following!";
+				marqueeWindow.AddAlert(alertText, "rgb(0,255,128)");
+			}
 		}
 		// Bits
 		if (line2.startsWith("Cheered ") && line2.indexOf(" bit") > -1) {
@@ -195,7 +212,7 @@ function ProcessActivityMessage(messageEl) {
 		// Raids
 		if (line2.startsWith("Raided you ")) {
 			let user = line1;
-			let alertText = user + " is raiding" + line2.replace("Raided you","") + "!";
+			let alertText = user + " is raiding" + line2.replace("Raided you", "") + "!";
 			marqueeWindow.AddAlert(alertText, "rgb(128,128,255)");
 		}
 		// Subscribe
@@ -207,9 +224,9 @@ function ProcessActivityMessage(messageEl) {
 		// Gift sub
 		if (line2.startsWith("Gave out ")) {
 			let user = line1;
-			let alertText = user + " " + line2.replace("Gave ","gave") + "!";
+			let alertText = user + " " + line2.replace("Gave ", "gave") + "!";
 			marqueeWindow.AddAlert(alertText, "rgb(255,0,128)");
 		}
 	}
-    messageEl.classList.add("processed");
+	messageEl.classList.add("processed");
 }
