d28745604958b3d21d9e11ff40b6e4f57632ded3
dobbsworks <63677372+dobbsworks@users.noreply.github.com>
2021-07-19 14:13:13 -0500

    Update Counter.js

--- Tools/Streambot/javascript/Counter.js
+++ Tools/Streambot/javascript/Counter.js
@@ -1,14 +1,15 @@
 function CounterCommand(input, response) {
-    let counters = StorageHandler.counters.values;
-    let targetCounter = counters.find(a => a.key.toLowerCase() === input.toLowerCase());
-    if (!targetCounter) {
-        targetCounter = {key: input.toLowerCase(), value: 1}; // skipping 1 to avoid pluralization problems
-        counters.push(targetCounter);
-    }
-    targetCounter.value = +(targetCounter.value) + 1;
-    StorageHandler.counters = counters;
-
-    let message = response.replace("$0", targetCounter.value);
+	return Command(input, () => { 
+        let counters = StorageHandler.counters.values;
+        let targetCounter = counters.find(a => a.key.toLowerCase() === input.toLowerCase());
+        if (!targetCounter) {
+            targetCounter = {key: input.toLowerCase(), value: 1}; // skipping 1 to avoid pluralization problems
+            counters.push(targetCounter);
+        }
+        targetCounter.value = +(targetCounter.value) + 1;
+        StorageHandler.counters = counters;
     
-	return Command(input, () => { return message }, commandPermission.all, commandDisplay.hidden);
+        let message = response.replace("$0", targetCounter.value);
+        return message 
+    }, commandPermission.all, commandDisplay.hidden);
 }
