9a5f22ac92cf2f4b65ed09956741bc2cad80222f
dobbsworks <63677372+dobbsworks@users.noreply.github.com>
2021-08-05 08:22:02 -0500

    cactus wheel icon

--- Tools/Streambot/javascript/WheelHandler.js
+++ Tools/Streambot/javascript/WheelHandler.js
@@ -66,7 +66,7 @@ function CommandWheelColor(user, args) {
 
 function CommandWheelPattern(user, args) {
     let username = user.username;
-    let allowed = ["star", "bowser", "heart", "club", "diamond", "spade", "checker", "dobbs", "triforce", "pokeball"];
+    let allowed = ["star", "bowser", "heart", "club", "diamond", "spade", "checker", "dobbs", "triforce", "pokeball", "cactus"];
     let allowedStr = allowed.join(', ');
     let errorMessage = `Include a pattern name (${allowedStr}) like this: !wheelpattern star`;
     let argPattern = args[0];
