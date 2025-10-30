8b939da5a1d6f583287e1e0beac2de3add82616b
dobbsworks <63677372+dobbsworks@users.noreply.github.com>
2025-10-30 09:48:59 -0500

    audio visualizer

--- Experiments/AudioVisualizer/app.js
+++ Experiments/AudioVisualizer/app.js
@@ -9,11 +9,22 @@ var analyzer = AUDIO.createAnalyser();
 analyzer.fftSize = 128;
 var bufferLength = analyzer.frequencyBinCount;
 var dataArray = new Uint8Array(bufferLength);
+var currentSong;
+
+window.onload = init;
+
+var targetLoopTimeInSeconds = 60 * 6;
 
 
 // Cache HTML elements
-var vid = document.getElementById('src-audio'),
-        vizArea = document.getElementById('viz');
+var audioSrc = document.getElementById('src-audio');
+var targetVolume = 0.125;
+audioSrc.volume = 0.125;
+audioSrc.addEventListener('ended', LoopAudio, false);
+document.body.appendChild(audioSrc);
+var songList = [];
+
+var vizArea = document.getElementById('viz');
 
 
 // Creates a set of divs with graded BG colors.
@@ -49,98 +60,104 @@ function update() {
 		$segs[i].style.height = (300 * segScale + 12) + "px"; 
     }
     if(++rot > 360) rot = 0;
+
+
+    if (isFading) {
+        let fadeSpeed = 0.001;
+        if (audioSrc.volume > fadeSpeed) audioSrc.volume -= fadeSpeed;
+        else audioSrc.volume = 0.0;
+    } 
+    if (targetLoopTimeInSeconds < accruedLoopTime + audioSrc.currentTime) {
+        NextSong();
+    }
 }
 
 
 function init() {
-
     document.body.style.opacity = 1.0;
-
-    // Connect video to analyzer and analyzer to audio-out
-    var source = AUDIO.createMediaElementSource(vid);
+    // Connect audio to analyzer and analyzer to audio-out
+    var source = AUDIO.createMediaElementSource(audioSrc);
     source.connect(analyzer);
     analyzer.connect(AUDIO.destination);
-    
     $segs = createSegments(bufferLength)
     loop();
+    InitializeControls();
 }
 
 
 // Main loop
 function loop() {
-
-    if (vid.ended) {
-        setTimeout(() => {
-            document.body.style.opacity = 0.0;
-        }, 0.5);
-    }
     requestAnimationFrame(loop);
     update();
-
 }
 
 
-// Kick it off when the video is playable
-var isStarted = false;
-// vid.addEventListener('loadeddata', Start, false);
-// document.body.addEventListener('click', Start, false);
-document.getElementById('songUpload').addEventListener('change', function(event){
-    var file = this.files[0];
-    var blob = window.URL || window.webkitURL;
-    var fileURL = blob.createObjectURL(file);
-    document.getElementById('src-audio').src = fileURL;
-    document.getElementById('src-audio').addEventListener('loadeddata', () => {
-        console.log("BEEP")
-    }, false);
-});
 
-document.getElementById('trackNameUpload').addEventListener('change', function(event){
-    document.getElementById('trackName').innerText = document.getElementById('trackNameUpload').value;
-});
+function InitializeControls() {
+    var audioElements = document.querySelectorAll("#audioContainer > *")
+    var controls = document.getElementById("controls");
+    for (let i = 0; i < audioElements.length; i++) {
+        let audioElement = audioElements[i];
 
-function ButtonStart() {
-    document.getElementById('page1').style.display = "none";
-    init();
-    document.getElementById("video").play();
-    setTimeout( () => {
-        AUDIO.resume();
-        vid.play();
-    }, 3000);
-}
-
-function Start() {
-    if (isStarted) return;
-    init();
-    
-    isStarted = true;
-
-    var params = new URL(window.location.href).searchParams;
-    var autoplay = params.get("autoplay")
-    if (autoplay) {
-        console.log("Autoplaying")
-        AUDIO.resume();
-        vid.play();
-    } else {
-        const playOverlay = document.createElement('div');
-        playOverlay.style.position = 'fixed';
-        playOverlay.style.backgroundColor = 'rgba(0,0,100,0.3)';
-        playOverlay.style.top = '0';
-        playOverlay.style.left = '0';
-        playOverlay.style.right = '0';
-        playOverlay.style.bottom = '0';
-        playOverlay.style.display = 'flex';
-        playOverlay.style.alignItems = 'center';
-        playOverlay.style.justifyContent = 'center';
-        const playBut = document.createElement('button');
-        playBut.textContent = 'Click to play';
-        playBut.style.padding = '1em';
-        playOverlay.appendChild(playBut);
-        playBut.addEventListener('click', (e) => {
-            playOverlay.remove();
-            AUDIO.resume();
-            vid.play();
-        });
-        document.body.appendChild(playOverlay);
+        let button = document.createElement("div");
+        button.innerText = audioElement.innerText;
+        if (audioElement.tagName == "AUDIO") {
+            songList.push(audioElement);
+            button.innerText = audioElement.dataset.name;
+            audioElement.volume = 0.25;
+            button.dataset.songIndex = i;
+            button.classList.add("controlButton");
+            button.onclick = () => {
+                PlaySong(audioElement);
+            }
+        }
+        controls.appendChild(button);
     }
+}
 
+function PlaySong(audioElement) {
+    var button = Array.from(document.querySelectorAll(".controlButton")).filter(a => a.innerText == audioElement.dataset.name)[0] ;
+
+    for (let el of document.querySelectorAll(".buttonPlaying")) {
+        el.classList.remove("buttonPlaying");
+    }
+    button.classList.add("buttonPlaying");
+
+    accruedLoopTime = 0.0;
+    audioSrc.src = audioElement.src;
+    AUDIO.resume();
+    audioSrc.play();
+    document.getElementById("video").play();
+    document.getElementById("trackName").innerText = audioElement.dataset.name;
+
+    let composer = "Cassidy’s Records";
+    if (audioElement.dataset.by) {
+        composer = audioElement.dataset.by;
+    }
+    document.getElementById("composer").innerText = composer;
+    
+    currentSong = audioElement;
+    audioSrc.currentTime = 0.0;
+}
+
+var accruedLoopTime = 0.0;
+function LoopAudio() {
+    if (currentSong.dataset.loops == "false") return;
+    accruedLoopTime += audioSrc.currentTime;
+    audioSrc.currentTime = parseFloat(currentSong.dataset.loopPoint);
+    audioSrc.play();
+}
+
+
+var isFading = false;
+function NextSong() {
+    isFading = true;
+
+    if (audioSrc.volume <= 0) {
+        audioSrc.volume = targetVolume;
+        var currentIndex = songList.indexOf(currentSong);
+        var newIndex = (currentIndex + 1) % songList.length;
+        PlaySong(songList[newIndex]);
+        isFading = false;
+    }
 }
